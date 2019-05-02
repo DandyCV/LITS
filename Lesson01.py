51122c06cde3abd8f4ab721d864306be1b074567
DandyCV <50106151+DandyCV@users.noreply.github.com>
2019-05-02 14:23:46 +0300

    Add files via upload

---
+++ Lesson01.py
@@ -0,0 +1,11 @@
+number_int = 11
+number_float = 10.5
+string = ('Hello')
+tuple1 = ( 1, 2, 3, 3, 5)
+list1 = ['a', 'b', 'c', 'a']
+dict1 = {'k1' : 'v1', 'k2' : 'v2', 'k3' : 'v3'}
+set1 = set(tuple1)
+bl = 1 == 1
+print ( number_int, number_float, string, tuple1, list1, dict1, set1, bl sep = '\n' )
+print (type(number_int), type(number_float), type(string), type(tuple1), type(list1), type(dict1), type(set1), type(bl) sep = '\n')
+input()
---
+++ Lesson02.py
@@ -0,0 +1,12 @@
+for i in range(101):
+    if i % 3 == 0 and i%5 == 0:
+        print (i , 'Fizz Buzz')
+    elif i % 3 == 0:
+        print ( i ,'Fizz')
+    elif i % 5 == 0:
+        print (str(i) + ' Buzz')
+list1 = {}
+if list1 is None:
+    print ('True')
+else:
+    print ('False')
