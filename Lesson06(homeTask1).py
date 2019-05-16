ad5b85fb29bcdc970a78e48eb9a292608a4dbb9b
DandyCV <dandycvc@gmail.com>
2019-05-17 00:43:54 +0300

    homework06

--- Lesson06(homeTask1).py
+++ Lesson06(homeTask1).py
@@ -1,10 +1,12 @@
-def sort_bub(list_num):
-    for i in range(len(list_num)-1):
-        for i in range(len(list_num)-1):
-            if list_num[i] > list_num[i+1]:
-                list_num[i], list_num[i+1] = list_num[i+1], list_num[i]
-            print (list_num)
-    return list_num
+def sort_bub(num):
+    n = 0
+    while n < len(num):
+        for i in range(len(num)-1):
+            if num[i] > num[i+1]:
+                num[i], num[i+1] = num[i+1], num[i]
+                print(num)
+        n += 1
+    return num
 
 
 list_num = [2, 7, 9, 3, 1, 8, 6, 4, 0, 5]
--- Lesson06(homeTask2).py
+++ Lesson06(homeTask2).py
@@ -1,13 +1,10 @@
-def sort_pas(list_num):
-    #for i in range(len(list_num)-1):
-        for i in range(len(list_num)-1):
-            if i != 0:
-                continue
-            while list_num[i] < list_num[i-1]:
-                    list_num[i], list_num[i-1] = list_num[i-1], list_num[i]
-                    i = i - 2
-                    print (list_num)
-    #return list_num
+def sort_pas(num):
+    for i in range(1, (len(num))):
+        while num[i] < num[i-1] and i > 0:
+                num[i], num[i-1] = num[i-1], num[i]
+                i -= 1
+                print(num)
+    return num
 
 
 list_num = [2, 7, 9, 3, 1, 8, 6, 4, 0, 5]
