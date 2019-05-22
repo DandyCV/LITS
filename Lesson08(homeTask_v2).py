9bf3754d24259bf8d807f06d314c9fbae6cd95a9
DandyCV <dandycvc@gmail.com>
2019-05-23 00:45:15 +0300

    homework08

--- Lesson08(homeTask_v2).py
+++ Lesson08(homeTask_v2).py
@@ -1,10 +1,14 @@
 def sort_1():
     list_1 = input('Через пробіл введіть числа для сортування: ')
     list_1 = list_1.split()
-    list_int = list(filter(lambda x: float(x) % 1 == 0, list_1))
-    list_float = list(filter(lambda x: float(x) % 1 != 0, list_1))
-    return sorted(list_int) + sorted(list_float)
+    list_int = []
+    list_float = []
+    for x in list(filter(lambda x: float(x) % 1 == 0, list_1)):
+        list_int.append(int(x))
 
+    for x in list(filter(lambda x: float(x) % 1 != 0, list_1)):
+        list_float.append(float(x))
+    return sorted(list_int) + sorted(list_float)
 
 
 print(sort_1())
