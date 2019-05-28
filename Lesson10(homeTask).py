2aa47314bbce7edaf0b0016f5e2e8b69da050255
DandyCV <dandycvc@gmail.com>
2019-05-28 20:17:00 +0300

    homework10

--- Lesson10(homeTask).py
+++ Lesson10(homeTask).py
@@ -1,50 +1,50 @@
-class Stek:
+class Stack:
     '''Стек глибино 5 елементів'''
-    def __init__(self, stek = []):
-        self.stek = stek
+    def __init__(self, stack = []):
+        self.stack = stack
 
     def push(self, new_element):
-        if len(self.stek) < 5:
+        if len(self.stack) < 5:
             print('Додаю в стек елемент ', new_element)
-            self.stek.append(new_element)
-            return self.stek
+            self.stack.append(new_element)
+            return self.stack
         else:
             print('Стек переповнений, елемент ', new_element, ' не додано.')
-            return self.stek
+            return self.stack
 
     @property
     def pop(self):
-        if self.stek:
-            print('Видаляю зі стеку елемент ', self.stek.pop())
-            return self.stek
+        if self.stack:
+            print('Видаляю зі стеку елемент ', self.stack.pop())
+            return self.stack
         else:
             return 'В стеку не залишилось елементів.'
 
     @property
     def length(self):
-        return 'Глибина стеку = ' + str(len(self.stek))
+        return 'Глибина стеку = ' + str(len(self.stack))
 
     @property
     def is_empty(self):
-        if self.stek:
+        if self.stack:
             return 'В стеку є елементи'
         else:
             return 'Стек порожній'
 
 
-print(Stek().push(2))
-print(Stek().push('a'))
-print(Stek().push(7))
-print(Stek().push('j'))
-print(Stek().push('11'))
-print(Stek().push('k'))
-print(Stek().pop)
-print(Stek().length)
-print(Stek().is_empty)
-print(Stek().pop)
-print(Stek().pop)
-print(Stek().pop)
-print(Stek().pop)
-print(Stek().pop)
-print(Stek().is_empty)
-print(Stek().pop)
+print(Stack().push(2))
+print(Stack().push('a'))
+print(Stack().push(7))
+print(Stack().push('j'))
+print(Stack().push('11'))
+print(Stack().push('k'))
+print(Stack().pop)
+print(Stack().length)
+print(Stack().is_empty)
+print(Stack().pop)
+print(Stack().pop)
+print(Stack().pop)
+print(Stack().pop)
+print(Stack().pop)
+print(Stack().is_empty)
+print(Stack().pop)
