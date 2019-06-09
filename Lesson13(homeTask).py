1722b21901dffd3733367b7bd9aebcef157873f4
DandyCV <dandycvc@gmail.com>
2019-06-09 20:24:44 +0300

    homeTask13

--- Lesson13(homeTask).py
+++ Lesson13(homeTask).py
@@ -1,12 +1,10 @@
 cycle_list = ('AB', 'CD')
 
 def cycle(*iterable):
-    iterable_list = [x for i in iterable for y in i for x in y]
+    iterable_list = [y for x in iterable for y in x]
     while True:
         for i in iterable_list:
-            yield i
-        iterable_list.reverse()
-
+            yield from i
 
 g = cycle(cycle_list)
 for i in g:
