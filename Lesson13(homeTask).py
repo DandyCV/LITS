630a395e1d732a71b4452c0f2e9594c5767e7540
DandyCV <dandycvc@gmail.com>
2019-06-10 18:47:44 +0300

    generator

--- Lesson13(homeTask).py
+++ Lesson13(homeTask).py
@@ -1,10 +1,10 @@
 cycle_list = ('AB', 'CD')
 
 def cycle(*iterable):
-    iterable_list = [y for x in iterable for y in x]
     while True:
-        for i in iterable_list:
-            yield from i
+        for i in iterable:
+            for j in i:
+                yield from j
 
 g = cycle(cycle_list)
 for i in g:
