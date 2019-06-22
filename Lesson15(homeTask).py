cba1d70f615bc5af00ce2b0b94934e21afb00508
DandyCV <dandycvc@gmail.com>
2019-06-22 19:51:14 +0300

    homeTask15

--- Lesson15(homeTask).py
+++ Lesson15(homeTask).py
@@ -9,8 +9,11 @@ def weather(city):
     r = requests.get(request)
     data = json.loads(r.text)
 
-    wind_d = round(data['wind']['deg']/45)              #визначаємо напрямок вітру
-    wind = {
+    if data['cod'] == '404':
+        print('Місто не знаедене. Можливо Ви ввели неправильно назву!')
+    else:
+        wind_d = round(data['wind']['deg']/45)              #визначаємо напрямок вітру
+        wind = {
         0 : "Північний",
         1 : "Північно-східний",
         2 : "Східний",
@@ -21,21 +24,21 @@ def weather(city):
         7 : "Північно-західний",
         8 : "Північний"
 }
-    wind_deg = wind[wind_d]                             
-    data['wind']['deg'] = wind_deg
+        wind_deg = wind[wind_d]
+        data['wind']['deg'] = wind_deg
 
-    press = round(data['main']['pressure']*0.750062)    #переводимо тиск в мм.рт.ст та округлюємо до цілих
-    data['main']['pressure'] = press
+        press = round(data['main']['pressure']*0.750062)    #переводимо тиск в мм.рт.ст та округлюємо до цілих
+        data['main']['pressure'] = press
 
-    temp = round(data['main']['temp']-273.15)           #переводимо температуру в °C та округлюємодо цілих
-    data['main']['temp'] = temp
+        temp = round(data['main']['temp']-273.15)           #переводимо температуру в °C та округлюємодо цілих
+        data['main']['temp'] = temp
 
-    print('''Пгода у місті {name}:
+        print('''Пгода у місті {name}:
 Стан неба - {weather[0][description]}
 Температура повітря - {main[temp]}°C
 Атмосферний тиск - {main[pressure]} мм.рт.ст
 Вологість повітря - {main[humidity]}%
 Швидкіть вітру - {wind[speed]} м/с
-Напрямок вітру - {wind[deg]}'''.format(**data))
+# Напрямок вітру - {wind[deg]}'''.format(**data))
 
-weather('Chernivtsi')
+weather('Moscow')
