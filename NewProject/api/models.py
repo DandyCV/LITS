fd9b4bd511c8588cb5f0d5c13d1b6d5e89cee2ed
DandyCV <dandycvc@gmail.com>
2019-07-15 20:54:17 +0300

    Lesson21

--- NewProject/api/models.py
+++ NewProject/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 dict_ = {
     "Monday": "Kyiv",
@@ -8,3 +9,14 @@ dict_ = {
     "Thursday": "Kharkiv",
     "Friday": "Chernivtsi"
 }
+
+class Article(models.Model):   #будуэ відображення запису в БД на обєкт пайтону
+    author = models.ForeignKey('auth.models.User', related_name='author', on_delete=models.CASCADE)
+    publish = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    title = models.TextField(max_length=128)
+    text = models.TextField(max_lengh=2000)
+
+    # class Meta:
+    #     table = 'article'   #назва таблиці
+    #     order_by = ['publish']    #сортування по колонці
