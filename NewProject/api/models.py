e8a64bc95aa6195993b1a7bb1807efb9d9866af1
DandyCV <dandycvc@gmail.com>
2019-07-17 23:35:36 +0300

    Lesson21(homeTask)

--- NewProject/api/models.py
+++ NewProject/api/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 # Create your models here.
 dict_ = {
     "Monday": "Kyiv",
@@ -20,3 +19,15 @@ class Article(models.Model):   #будуэ відображення запису
     # class Meta:
     #     table = 'article'   #назва таблиці
     #     order_by = ['publish']    #сортування по колонці
+
+
+class Comment(models.Model):
+    class Meta():
+        db_table = 'comment'
+        verbose_name = 'Comment'
+        verbous_name_plural = 'Comments'
+
+    user = models.ForeignKey('auth.models.User', related_name='author', on_delete=models.CASCADE)
+    text = models.TextField('Comment Text')
+    create = models.DateTimeField('add', auto_now_add=True)
+    moder = models.BooleanField(default=False)
